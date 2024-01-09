799bd3182c1404d749e338273874bfb17c885dec
Sandeep Agrawal <154302999+Sandeep08011999@users.noreply.github.com>
2024-01-09 14:45:21 +0530

    Add files via upload

---
+++ Online Examination System( practice-quiz)-1/js-it/quiz.js
@@ -0,0 +1,156 @@
+var score=0;//set score to 0
+var total=10;// total number of question
+var point=1;// point per correct answer
+var hightest=total*point;
+var x;
+var i;
+var t=0;
+  //Initializer
+  function init()
+  {
+	  //set correct answer
+	  sessionStorage.setItem('a1','d');
+	  sessionStorage.setItem('a2','b');
+	  sessionStorage.setItem('a3','c');
+	  sessionStorage.setItem('a4','a');
+	  sessionStorage.setItem('a5','b');
+	  sessionStorage.setItem('a6','b');
+	  sessionStorage.setItem('a7','a');
+	  sessionStorage.setItem('a8','a');
+	  sessionStorage.setItem('a9','d');
+	  sessionStorage.setItem('a10','a');
+  }
+$(document).ready(function(){
+	//Hide all question
+	  $('.questionForm').hide();
+	//Show first question
+	  $('#q1').show();  
+	//Question Next
+$('.questionForm #submit').click(function()
+      {
+		//Get data attributes
+        current=$(this).parent('form:first').data('question');
+        next=$(this).parent('form:first').data('question')+1;
+		//hide a question
+		$('.questionForm').hide();
+		//show next question
+		$('#q'+next+'').fadeIn(300);
+		process(''+current+'');
+		if(next==11)
+		{
+			$('#q1').show();
+		}
+		return false;
+	  });
+	//Question previous
+$('.questionForm #previous').click(function()
+      {
+		//Get data attributes
+        current=$(this).parent('form:first').data('question');
+        pre=$(this).parent('form:first').data('question')-1;
+		//hide a question
+		$('.questionForm').hide();
+		//show previous question
+		$('#q'+pre+'').fadeIn(300);
+		process(''+current+'');
+		if(pre==0)
+		{
+			$('#q10').show();
+		}
+		return false;
+	  });
+});
+//process the answer
+	function process(n)
+	{
+		//get input value
+		var submitted=$('input[name=q'+n+']:checked').val();
+		if(submitted==sessionStorage.getItem('a'+n+''))
+			{
+				//score=score+point;
+			}	
+		if(n==total)
+		{
+			//$('#results').html('<h3>Your final score is: '+score+' out of '+hightest+'</h3> <a href="paper.html"> take quiz again</a>');
+		}
+		return false;
+	}	
+//add event listener
+ window.addEventListener('load',init,false);
+// clicked Question color change
+function f1(x)
+{ var j;
+ for(i=1;i<=10;i=i+1)
+ {
+	 if(i==x )
+	 {   
+		document.getElementById('q'+i+'').style.display='block';
+        document.getElementById('a'+i+'').style.backgroundColor = '#3a5909';	
+        document.getElementById('a'+i+'').style.color = white;
+	 }
+	 else
+	 {
+		 document.getElementById('q'+i+'').style.display='none'; 
+	 }
+ }
+}
+// Show Question (jump on the question) 
+function f2(x)
+{ var j;
+ for(i=1;i<=10;i=i+1)
+ {
+	 if(i==x )
+	 {   
+		document.getElementById('q'+i+'').style.display='block';	
+	 }
+	 else
+	 {
+		 document.getElementById('q'+i+'').style.display='none'; 
+	 }
+ }
+}
+// marked Question  for review 
+function f3(x)
+{ var j;
+ for(i=1;i<=10;i=i+1)
+ {
+	 if(i==x )
+	 {   
+		document.getElementById('q'+i+'').style.display='block';
+        document.getElementById('a'+i+'').style.backgroundColor = 'purple';	
+	 }
+	 else
+	 {
+		 document.getElementById('q'+i+'').style.display='none'; 
+	 }
+ }
+}
+/*function finish1()
+{
+	$('#results').html('<h3>Your final score is: '+score+' out of '+hightest+'</h3> <a href="paper.html"> take quiz again</a>');
+}*/
+//process the answer of a question;
+	function f4()
+	{ 
+	  var n;
+	  
+		for(n=1;n<=10;n++)
+		{
+		//get input value
+		var submitted=$('input[name=q'+n+']:checked').val();
+		if(submitted==sessionStorage.getItem('a'+n+''))
+			{
+				score=score+point;
+			}	
+        else
+		   {
+			   score=score+0; 
+		   }		
+		}
+		if(t==0)
+		{
+			t=t+1;
+			$('#results').html('<h3>Your final score is: '+score+' out of '+hightest+'</h3> <a href="paper.html"></a>')
+		}
+		return false;
+	}
